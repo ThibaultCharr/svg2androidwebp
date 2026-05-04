4aaa31efed6a73eeef943284aa6d4f13a7be6c39
ThibaultCharr <thibault.charriere@gmail.com>
2026-05-04 13:33:30 +0200

    feat: add dependency check to converter

--- converter.py
+++ converter.py
@@ -1,1 +1,11 @@
-# SVG to Android WebP converter
+import shutil
+
+
+def check_dependencies():
+    """Returns a list of missing tool names, empty if all present."""
+    missing = []
+    if not shutil.which("rsvg-convert"):
+        missing.append("librsvg")
+    if not shutil.which("cwebp"):
+        missing.append("webp")
+    return missing
